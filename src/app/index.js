9b5c1410582d0b48a193e9993c8c6ad333aaec31
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2024-09-26 21:41:07 -0400

    Link stylesheets to ejs files

--- src/app/index.js
+++ src/app/index.js
@@ -13,6 +13,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../", "views"));
 
+// middleware
+app.use(express.static(path.join(__dirname, "../../", "public")))
+
 // routes
 app.use("/", homeRoute);
 
