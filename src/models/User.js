9494783d1acd5da1421d251420a0d7a3fe9323a2
Coleman Matey <113218526+colemanmatey@users.noreply.github.com>
2024-09-29 02:57:23 -0400

    Add a model for the User table

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+// modules
+const sequelize = require("../app/db");
+const { userSchema } = require("../schemas");
+
+// model
+const User = sequelize.define("User", userSchema, {
+    tableName: "User", // existing table name
+    timestamps: false,
+});
+
+// exports
+module.exports = User;
